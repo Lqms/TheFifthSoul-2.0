740c320c3d6cf2d2ff4cf3128107465962a6613c
Luqmus <rjcnz1007@yandex.ru>
2023-04-04 12:41:55 +0400

    1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _dashCooldown = 3;
 
+    public bool CanDash { get; private set; } = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && CanDash == true)
+        {
+            StartCoroutine(DashReloading());
+        }
+    }
+
+    private IEnumerator DashReloading()
+    {
+        yield return new WaitForSeconds(0.01f);
+        print("dash start reload");
+        CanDash = false;
+
+        yield return new WaitForSeconds(_dashCooldown);
+
+        CanDash = true;
+        print("dash is ready");
+    }
 }
