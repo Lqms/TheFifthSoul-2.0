6e83bbe02b040c8ed617c9e8ecd7d0c0bcba270d
Luqmus <rjcnz1007@yandex.ru>
2023-04-27 17:54:53 +0400

    1

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private Player _player;
 
     public Player Player => _player;
+
+    public event UnityAction PlayerInRange;
+    public event UnityAction PlayerOutOfRange;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            PlayerInRange?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            PlayerOutOfRange?.Invoke();
+        }
+    }
 }
