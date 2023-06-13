a638270002a8e137434cbb2aea41c727b7865e13
Lobsterr_ <63951046+ssdd22@users.noreply.github.com>
2023-06-13 17:01:10 +0800

    1
    1

---
+++ Assets/Scripts/Objects/DeathScreen.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScreen : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private Image _image;
+
+    private void OnEnable()
+    {
+        _player.Died += OnPlayerDied;
+    }
+
+    private void OnDisable()
+    {
+        _player.Died -= OnPlayerDied;
+    }
+
+    private void OnPlayerDied()
+    {
+        
+    }
+}
